848b84d30cdc9a2ae8eb4b750007826d6ab4c9a4
plumpaxton <77244775+plumpaxton@users.noreply.github.com>
2021-05-14 18:17:07 -0400

    Add files via upload
    Added code for temperature conversion

---
+++ temperature_conversion.py
@@ -0,0 +1,7 @@
+# Get input and place it in a variable named degree
+degree = int(input("enter a degree in celsius: "))
+
+# Make a calculation that turns a number from celsius to Fahrenheit
+fah = degree + 32
+
+print("the temperature in fahrenheit is: ", fah)
